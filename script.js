f9cbe34da28d7b3b950d9186eb1389c9240c2d40
EmilyRight <emilya.korkunova@gmail.com>
2021-09-20 21:02:29 +0300

    feat: add up-button

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-
 const burgerBtn = document.querySelector(".burger-menu");
 
 if (burgerBtn) {
     const menu = document.querySelector(".header-menu-list");
     burgerBtn.addEventListener("click", function(e) {
-        burgerBtn.toggle("active")
+        burgerBtn.classList.toggle("active");
+        menu.classList.toggle("active");
     })
 }
+
+
+let upButton = document.querySelector('.up-button');
+
+window.onscroll = function () {
+  if (window.pageYOffset > 200) {
+    upButton.classList.add('shown');
+  } else {
+    upButton.classList.remove('shown');
+  };
+
+};
+
+upButton.onclick = function () {
+  window.scrollTo(0, 0);
+};
